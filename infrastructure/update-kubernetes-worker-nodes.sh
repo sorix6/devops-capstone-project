60556005c40aad08455403bba7178f7f9805c89d
Sorina Dumitrescu <elisa_s_d@yahoo.com>
2019-12-16 00:03:32 +0000

    Fixed naming error on stack

--- infrastructure/update-kubernetes-worker-nodes.sh
+++ infrastructure/update-kubernetes-worker-nodes.sh
@@ -1,5 +1,5 @@
 aws cloudformation update-stack \
---stack-name=capstone-worker-nodes  \
+--stack-name=capstone-project-worker-nodes  \
 --parameters=file://${1} \
 --template-body=file://${2} \
 --region=us-west-2 \
